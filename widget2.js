b05168f08a52f9b1f00fe21ecde79c622e5c246a
DeemaJS <deeema@outlook.com>
2015-10-31 15:54:44 +0200

    Last job add package.json

--- widget2.js
+++ widget2.js
@@ -1,28 +1,28 @@
 var fs = require('fs');
 var domify = require('domify');
 var insertCss = require('insert-css');
-var EventEmitter = require('events').EventEmitter; 
 var inherits = require('inherits');
+var EventEmitter = require('events').EventEmitter;
 
 var html = fs.readFileSync('./widget.html', 'utf8');
 var css = fs.readFileSync('./widget.css', 'utf8');
 insertCss(css);
-                                                   
-module.exports = Widget;                           
-inherits(Widget, EventEmitter);                    
-                                                   
+
+module.exports = Widget;
+inherits(Widget, EventEmitter);
+
 function Widget () {
-	var self = this;                               
+  var self = this;
   if (!(this instanceof Widget)) return new Widget;
   var form = this.element = domify(html);
-
+  
   form.addEventListener('submit', function (ev) {
-  	ev.preventDefault();
-  	var msg = form.querySelector('textarea[name="msg"]').value;
-  	self.emit('message', msg);
+    ev.preventDefault();
+    var msg = form.querySelector('textarea[name="msg"]').value;
+    self.emit('message', msg);
   });
-}    
+}
 
 Widget.prototype.appendTo = function (target) {
-	target.appendChild(this.element);
-};                                            
+  target.appendChild(this.element);
+};                                           
